628cd5c1a7a7326362dee82dbb8d58668c0d585f
joaoguirqueiroz <joaoguirqueiroz@gmail.com>
2026-06-05 20:43:51 -0300

    inico do codigo com as funçoes de limpar o terminal e importar data e hora.

--- README.md
+++
@@ -1,2 +0,0 @@
-# trabalho-de-python-SR2
-ESTE É UM PROJETO DE: Sistema de Planejamento de Eventos
---
+++ codinicial.py
@@ -0,0 +1,7 @@
+import os
+import datetime
+
+eventos = []
+
+def limpar_tela():
+    os.system("cls")
